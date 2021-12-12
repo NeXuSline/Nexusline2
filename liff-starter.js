0e99f96b8df63840c9895603318ecd9704b665a8
NeXuSline <95968922+NeXuSline@users.noreply.github.com>
2021-12-12 05:18:59 +0200

    Add files via upload

---
+++ liff-starter.js
@@ -0,0 +1,96 @@
+window.onload = function() {
+    const useNodeJS = false; 
+    const defaultLiffId = "1655790923-WwjkqNYY";
+    let myLiffId = "";
+
+    if (useNodeJS) {
+        fetch('/liff/send-id', {
+            headers: {
+                "referer": "https://pixiv.net/en/"
+            }
+        })
+            .then(function(reqResponse) {
+                return reqResponse.json();
+            })
+            .then(function(jsonResponse) {
+                myLiffId = jsonResponse.id;
+                initializeLiff(myLiffId);
+            })
+            .catch(function(error) {
+                console.log(error.message, error.code);
+            });
+    } else {
+        myLiffId = defaultLiffId;
+        initializeLiff(myLiffId);
+    }
+};
+
+function initializeLiff(myLiffId) {
+    liff
+        .init({
+            liffId: myLiffId
+        })
+        .then(() => {
+            sendLiff();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function sendLiff(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'text') {
+        liff.sendMessages([{
+            type: 'text',
+            text: getParameterByName('text')
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    } else if (tipe === 'image') {
+        liff.sendMessages([{
+            type: 'image',
+            originalContentUrl: getParameterByName('img'),
+            previewImageUrl: getParameterByName('img')
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    } else if (tipe === 'video') {
+        prev = getParameterByName('piu');
+        if(prev !== null && prev !== '') {
+            dura = prev;
+        } else {
+            dura = "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e24ff770303293.5b9f85b588dcb.gif";
+        }
+        liff.sendMessages([{
+            type: 'video',
+            originalContentUrl: getParameterByName('ocu'),
+            previewImageUrl: dura
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    } else if (tipe === 'audio') {
+        duration = getParameterByName('duration');
+        if(duration !== null && duration !== '') {
+            dura = parseInt(duration);
+        } else {
+            dura = 60000;
+        }
+        liff.sendMessages([{
+            type: 'audio',
+            originalContentUrl: getParameterByName('link'),
+            duration: dura
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
